d0c440e59b7f085ccc9f06164144640f57fbe5e8
Adharsh M <48812285+adharshmk96@users.noreply.github.com>
2024-07-10 09:35:29 +0700

    Update utils.sh

--- bash/utils.sh
+++ bash/utils.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-git ls-files | rsync -avz --files-from=- . user@host:target_dir
+USERNAME="root"
+IP="0.0.0.0"
 
+# rsync files
+git ls-files | rsync -avz --files-from=- ./ $USERNAME@$IP:sharepoint_storage_sync
+
+# postgresql run sql script
+docker container exec -it postgresdb psql -U postgresuser -d postgresuser -f truncate.sql
 
